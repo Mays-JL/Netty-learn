e0faf12db92fda5ecde94ee71663ce2073b933d7
linlin20ai <3357929671@qq.com>
2025-11-04 11:17:30 +0800

    feat: 基础入门1-hi NettyServer

---
+++ src/main/java/maysjl/com/cn/nettydemo/hiNetty/NettyServer.java
@@ -0,0 +1,43 @@
+package maysjl.com.cn.nettydemo.hiNetty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @program: netty-demo
+ * @description: 一个简单的Netty服务端示例
+ * @author: May's_JL
+ * @create: 2025-11-04 10:55
+ **/
+public class NettyServer {
+    public static void main(String[] args) {
+        new NettyServer().bing(7397);
+    }
+
+    public void bing(int port){
+        // 配置服务器端NIO线程组
+        //NioEventLoopGroup extends MultithreadEventLoopGroup Math.max(1, SystemPropertyUtil.getInt("io.netty.eventLoopThreads", NettyRuntime.availableProcessors() * 2));
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup childGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, childGroup)
+                    //非阻塞模式
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childHandler(new MyChannelInitializer());
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            childGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+
+    }
+}
