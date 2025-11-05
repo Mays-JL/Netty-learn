734f4af31df0e3572f5ca3d87d0d783e45851eac
linlin20ai <3357929671@qq.com>
2025-11-05 19:05:27 +0800

    feat: 基础入门10-关于ChannelOutboundHandlerAdapter简单使用

---
+++ src/main/java/maysjl/com/cn/nettydemo/hiNettyDiy/client/MyOutMsgHandler.java
@@ -0,0 +1,26 @@
+package maysjl.com.cn.nettydemo.hiNettyDiy.client;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+
+/**
+ * @program: netty-demo
+ * @description:
+ * @author: May's_JL
+ * @create: 2025-11-04 23:45
+ **/
+public class MyOutMsgHandler extends ChannelOutboundHandlerAdapter {
+
+    @Override
+    public void read(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush("ChannelOutboundHandlerAdapter.read 发来一条消息\r\n");
+        super.read(ctx);
+    }
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+        ctx.writeAndFlush("ChannelOutboundHandlerAdapter.write 发来一条消息\r\n");
+        super.write(ctx, msg, promise);
+    }
+}
