7d1bc3ec37a37b40aa0d041fbb8f324b0fdd14cd
linlin20ai <3357929671@qq.com>
2025-11-06 11:30:46 +0800

    feat: 中级1-Netty与SpringBoot整合

---
+++ src/main/java/maysjl/com/cn/nettydemo/server/NettyServer.java
@@ -0,0 +1,66 @@
+package maysjl.com.cn.nettydemo.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @program: netty-demo
+ * @description:
+ * @author: May's_JL
+ * @create: 2025-11-06 10:46
+ **/
+@Component("nettyServer")
+public class NettyServer {
+
+    private Logger logger = LoggerFactory.getLogger(NettyServer.class);
+
+    //配置服务端NIO线程组
+    private final EventLoopGroup parentGroup = new NioEventLoopGroup();
+    private final EventLoopGroup childGroup = new NioEventLoopGroup();
+    private Channel channel;
+
+    public ChannelFuture bing(InetSocketAddress address){
+        ChannelFuture channelFuture = null;
+        try{
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(parentGroup, childGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childHandler(new MyChannelInitializer());
+
+            channelFuture = b.bind(address).syncUninterruptibly();
+            channel = channelFuture.channel();
+        }catch (Exception e){
+            logger.error(e.getMessage());
+        }finally {
+            if (null != channelFuture && channelFuture.isSuccess()) {
+                logger.info("maysjl.com.cn.nettydemo.server.NettyServer start done");
+            }else {
+                logger.error("maysjl.com.cn server start error");
+            }
+        }
+        return channelFuture;
+    }
+
+
+    public void destroy(){
+        if (null == channel) return;
+        channel.close();
+        parentGroup.shutdownGracefully();
+        childGroup.shutdownGracefully();
+    }
+
+    public Channel getChannel(){
+        return channel;
+    }
+}
